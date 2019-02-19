0a3c20bf9530f678324affc3b074582a89c18795
Nyagajrr <muchokijnrbenson@gmail.com>
2019-02-19 11:56:34 +0300

    login form and its methods

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -3,3 +3,9 @@ from wtforms import StringField,PasswordField, BooleanField, SubmitField
 from wtforms.validators import Required,Email,EqualTo
 from ..models import User
 from wtforms import ValidationError
+
+class LoginForm(FlaskForm):
+    email = StringField('Enter your Email', validators=[Required(), Email()])
+    password = PasswordField('Enter Password',validators=[Required()])
+    remember = BooleanField('Remember me')
+    submit = SubmitField('Sign In')
