7228545de61482c8bfdf30a55c37ff107da59ba5
Nyagajrr <muchokijnrbenson@gmail.com>
2019-02-19 12:37:36 +0300

    routing to main page

--- app/main/views.py
+++ app/main/views.py
@@ -16,3 +16,24 @@ def index():
     title = 'blooger.com'
 
     return render_template("index.html", title=title)
+@main.route('/Interview',methods = ['GET', 'POST'])
+@login_required
+def Interview():
+
+    pitch_form = PitchFormI()
+
+    if pitch_form.validate_on_submit():
+        pitch = pitch_form.pitch.data
+        # cat = pitch_form.my_category.data
+
+        new_pitch = Pitch(pitch_content=pitch,user = current_user)
+        new_pitch.save_pitch()
+
+        #return redirect(url_for('index.html'))
+
+    all_pitches = Pitch.get_category('Interview')
+
+    title = 'Interview Pitch'
+
+    return render_template("Interview.html", pitch_form = pitch_form, pitches = all_pitches)
+
