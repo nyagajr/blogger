189c0a32039e3a8cb86164a083965a9e30be223b
Nyagajrr <muchokijnrbenson@gmail.com>
2019-02-19 12:02:01 +0300

    creating app instances

--- manage.py
+++ manage.py
@@ -3,6 +3,13 @@ from flask_script import Manager,Server
 from flask_migrate import Migrate, MigrateCommand
 from app.models import User, Pitch, Comment
 
+# instances for the create_app
+#app = create_app('test')
+app = create_app('development')
+# app = create_app('production')
+
+manager = Manager(app)
+manager.add_command('server',Server)
 migrate = Migrate(app,db)
 manager.add_command('db',MigrateCommand)
 
