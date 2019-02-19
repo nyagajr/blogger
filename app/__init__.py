dbb4a4316f3de471c60fb3fd4460cf82b4c1bfab
Nyagajrr <muchokijnrbenson@gmail.com>
2019-02-19 11:52:41 +0300

    creating instances

--- app/__init__.py
+++ app/__init__.py
@@ -6,3 +6,26 @@ from flask_uploads import UploadSet, configure_uploads, IMAGES
 from flask_mail import Mail
 from flask_simplemde import SimpleMDE
 from config import config_options
+login_manager = LoginManager()
+login_manager.session_protection = 'strong'
+login_manager.login_view = 'auth.login'
+
+bootstrap = Bootstrap()
+db = SQLAlchemy()
+mail = Mail()
+photos = UploadSet('photos',IMAGES)
+simple = SimpleMDE()
+
+def create_app(config_name):
+
+    app = Flask(__name__)
+
+
+    # app configurations
+    app.config.from_object(config_options[config_name])
+
+
+    # initializing flask extensions
+    bootstrap.init_app(app)
+    db.init_app(app)
+    mail.init_app(app)
