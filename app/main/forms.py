3c60c828d1250e85a7e319d9b0a7c0df2ed2d800
Nyagajrr <muchokijnrbenson@gmail.com>
2019-02-19 12:06:32 +0300

    creating form instances and methods

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,3 +1,21 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, TextAreaField, SubmitField, SelectField
 from wtforms.validators import Required
+class ReviewForm(FlaskForm):
+
+    title = StringField('Review title',validators=[Required()])
+    review = TextAreaField('Movie review', validators=[Required()])
+    submit = SubmitField('Submit')
+
+class CommentForm(FlaskForm):
+    comment = TextAreaField('Comment', validators=[Required()])
+    submit = SubmitField('add Comment')
+
+
+class PitchFormL(FlaskForm):
+    pitch = TextAreaField('add blog', validators=[Required()])
+    submit = SubmitField('Submit')
+
+class PitchFormP(FlaskForm):
+    pitch = TextAreaField('Pitch Comment', validators=[Required()])
+    my_category = SelectField('Category', choices=[('Promotion','Promotion')])
