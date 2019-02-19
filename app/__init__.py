69a14f3cdb099758ccbf4a976e8088adf735dbdb
Nyagajrr <muchokijnrbenson@gmail.com>
2019-02-19 11:53:04 +0300

    creating instances

--- app/__init__.py
+++ app/__init__.py
@@ -6,6 +6,7 @@ from flask_uploads import UploadSet, configure_uploads, IMAGES
 from flask_mail import Mail
 from flask_simplemde import SimpleMDE
 from config import config_options
+
 login_manager = LoginManager()
 login_manager.session_protection = 'strong'
 login_manager.login_view = 'auth.login'
@@ -29,3 +30,17 @@ def create_app(config_name):
     bootstrap.init_app(app)
     db.init_app(app)
     mail.init_app(app)
+    login_manager.init_app(app)
+    simple.init_app(app)
+
+    # Registering the main app Blueprint
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+
+    # Registering the auth blueprint
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint,url_prefix = '/auth')
+
+    configure_uploads(app,photos)
+
+    return app
