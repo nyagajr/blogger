7a5357c83f7e6eb22f6696d5fcd961ff494b7b1a
Nyagajrr <muchokijnrbenson@gmail.com>
2019-02-19 12:36:54 +0300

    routing

--- app/main/views.py
+++ app/main/views.py
@@ -3,3 +3,16 @@ from . import main
 from .forms import ReviewForm , PitchFormL, CommentForm
 from ..models import User, Pitch, Review,Comment
 from flask_login import login_required, current_user
+
+# Views
+@main.route('/')
+def index():
+    '''
+    View root page function that returns the index page and its data
+    '''
+
+
+
+    title = 'blooger.com'
+
+    return render_template("index.html", title=title)
