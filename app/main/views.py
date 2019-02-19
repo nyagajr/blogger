ce34d7421a81645ff675fa4df02ae9a429e4a107
Nyagajrr <muchokijnrbenson@gmail.com>
2019-02-19 12:38:30 +0300

    routing

--- app/main/views.py
+++ app/main/views.py
@@ -37,3 +37,66 @@ def Interview():
 
     return render_template("Interview.html", pitch_form = pitch_form, pitches = all_pitches)
 
+@main.route('/Pickupline',methods = ['GET', 'POST'])
+@login_required
+def Pickupline():
+
+    pitch_form = PitchFormL()
+
+    if pitch_form.validate_on_submit():
+        pitch = pitch_form.pitch.data
+        # cat = pitch_form.my_category.data
+
+        new_pitch = Pitch(pitch_content=pitch,user = current_user)
+        new_pitch.save_pitch()
+
+        #return redirect(url_for('index.html'))
+
+    all_pitches = Pitch.get_all_pitches()
+
+    title = 'Pickupline Pitch'
+
+    return render_template("Pickupline.html", pitch_form = pitch_form, pitches = all_pitches)
+
+@main.route('/Promotion',methods = ['GET', 'POST'])
+@login_required
+def Promotion():
+
+    pitch_form = PitchFormP()
+
+    if pitch_form.validate_on_submit():
+        pitch = pitch_form.pitch.data
+        cat = pitch_form.my_category.data
+
+        new_pitch = Pitch(pitch_content=pitch, pitch_category = cat, user = current_user)
+        new_pitch.save_pitch()
+
+        #return redirect(url_for('index.html'))
+
+    all_pitches = Pitch.get_category('Promotion')
+
+    title = 'Promotion Pitch'
+
+    return render_template("Promotion.html", pitch_form = pitch_form, pitches = all_pitches)
+
+@main.route('/pitch/<int:id>',methods = ['GET','POST'])
+@login_required
+def pitch(id):
+
+    my_pitch = Pitch.query.get(id)
+    comment_form = CommentForm()
+
+    if id is None:
+        abort(404)
+
+    if comment_form.validate_on_submit():
+        comment_data = comment_form.comment.data
+        new_comment = Comment(comment_content = comment_data, pitch_id = id, user = current_user)
+        new_comment.save_comment()
+
+        return redirect(url_for('main.pitch',id=id))
+
+    all_comments = Comment.get_comments(id)
+
+    title = 'Comment Section'
+    return render_template('pitch.html',pitch = my_pitch, comment_form = comment_form, comments = all_comments, title = title)
